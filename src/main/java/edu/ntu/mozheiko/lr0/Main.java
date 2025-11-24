d890da4146ac87917b33d32eb13692f082f7aa36
MozheikoDaria333 <pepega489@gmail.com>
2025-11-24 15:50:55 +0200

    Initial commit: lab1

--- src/main/java/edu/ntu/mozheiko/lr0/Main.java
+++
@@ -1,7 +0,0 @@
-package edu.ntu.mozheiko.lr0;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
---
+++ src/main/java/edu/ntu/mozheiko/lr1/Main.java
@@ -0,0 +1,7 @@
+package edu.ntu.mozheiko.lr1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world from LR1 branch");
+    }
+}
