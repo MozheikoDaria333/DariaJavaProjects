5d17a5bc7550f909bb695a9394b842f1aae83a1b
MozheikoDaria333 <79278423+MozheikoDaria333@users.noreply.github.com>
2025-11-28 13:02:16 +0200

    Update DbConnectionManager.java

--- src/main/java/edu/ntu/mozheiko/lr5/util/DbConnectionManager.java
+++ src/main/java/edu/ntu/mozheiko/lr5/util/DbConnectionManager.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DbConnectionManager {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/students_db";
+    private static final String URL = "jdbc:postgresql://localhost:5432/mozheiko_db";
     private static final String USER = "postgres";
     private static final String PASSWORD = "1234"; //
 
